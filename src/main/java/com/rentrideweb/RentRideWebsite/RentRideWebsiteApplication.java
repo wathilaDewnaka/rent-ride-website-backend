2d7fdeb76e29efcd2501956ab4ecd866709dfaae
Wathila Dewnaka Weerakoon Karunathilake <165549226+wathilaDewnaka@users.noreply.github.com>
2024-10-17 12:09:43 +0530

    Update RentRideWebsiteApplication.java

--- src/main/java/com/rentrideweb/RentRideWebsite/RentRideWebsiteApplication.java
+++ src/main/java/com/rentrideweb/RentRideWebsite/RentRideWebsiteApplication.java
@@ -1,6 +1,5 @@
 package com.rentrideweb.RentRideWebsite;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
